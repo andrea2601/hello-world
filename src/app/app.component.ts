c841fa29fd776616caacee20809d6f4d32444e58
andrea2601 <andrealogiudice26@gmail.com>
2022-10-10 15:14:47 +0200

    feat: BE and http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,71 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from './services/service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hello-world';
   filter = "";
-  obj = [
-    {
-    name: "Andrea",
-    surname: "Lo Giudice",
-    age: "24",
-    location: "Palermo"
-    },
-    {
-    name: "Marco",
-    surname: "Rossi",
-    age: "27",
-    location: "Milano"
-    },
-    {
-    name: "Giulio",
-    surname: "Genova",
-    age: "32",
-    location: "Milano"
-    },
-    {
-    name: "Genoveffa",
-    surname: "Romano",
-    age: "24",
-    location: "Roma"
-    },
-    {
-    name: "Giovanni",
-    surname: "Giannone",
-    age: "18",
-    location: "Catania"
-    },
-    {
-    name: "John",
-    surname: "Brown",
-    age: "45",
-    location: "Londra"
-    },
-    {
-    name: "Claudia",
-    surname: "Rossi",
-    age: "18",
-    location: "Parigi"
-    },
-    {
-    name: "Mery",
-    surname: "Verdi",
-    age: "56",
-    location: "Palermo"
-    },
-    {
-    name: "Giuseppe",
-    surname: "Peppe",
-    age: "88",
-    location: "Lucca"
-    },
-]
+  obj: any = []
+  constructor(public service: ServiceService) { }
 
-recivedFilter(value: string){
-  this.filter=value;
-}
+  ngOnInit(): void {
+    this.service.loadPeople().subscribe((data: any) => {
+      this.obj = data;
+      this.service.loading = false;
+    });
+
+  }
+
+  recivedFilter(value: string) {
+    this.filter = value;
+  }
 }
--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+  constructor(public http: HttpClient) { }
 
-  constructor() { }
+  loading: boolean = false;
+
+  loadPeople() {
+    this.loading = true;
+    return this.http.get("http://localhost:3000/people");
+  }
 }
